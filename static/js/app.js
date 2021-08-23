8654eb616506b5b268f57edcfba99f23eaa27bc1
Jacob Zebker <jacobzebker@jacobs-air.attlocal.net>
2021-08-22 17:39:20 -0700

    list for drop down

--- static/js/app.js
+++ static/js/app.js
@@ -23,6 +23,13 @@ function buildTable(data) {
   });
 }
 
+// Populate drop down list of choices for shape column
+let shapeValues = [];
+data.forEach((dataRow) =>{
+  shapeValues.push(dataRow["shape"])
+});
+console.log(new Set(shapeValues));
+
 // 1. Create a variable to keep track of all the filters as an object.
 var filters = {};
 
